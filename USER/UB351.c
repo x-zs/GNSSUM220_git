03d26dd338fb87fd8ab27c43672a02feeb2e8ccb
jason xiao <741870940@qq.com>
2017-09-14 12:00:03 +0800

    Revert "Revert "jason xiao""
    This reverts commit ce6da718f4bf67f00ab4d6ae6b16984c1c4d0e95.

--- USER/UB351.c
+++ USER/UB351.c
@@ -14,6 +14,7 @@ u8 Status_Msg[Reser_Len]={0,0xff,0xff,0xff};  //保留字 帧序号+3字节保留
 void UB351_Transmit(u8 *message,u16 length,u8 Frame_type)
 {	u8 Frame_check=0;
 	u16 i=0;
+
 	Frame_Len=length+Reser_Len;   //观测量载荷+状态信息载荷+校验位
 	Frame_Head[11]=(Frame_Len& 0xFF00)>>8; //长度高字节
 	Frame_Head[10]=(u8)(Frame_Len&0xFF);    //长度低字节
--- USER/UB351.h
+++ USER/UB351.h
@@ -4,6 +4,7 @@
 #include "stdio.h"	
 #define Station_Number_H (512& 0xFF00)>>8
 #define Station_Number_L (u8)(512&0xFF)
+#define Station_Number 512
 #define Station_Type 3
 #define Reser_Len 4  //±£Áô×Ö³¤¶È
 extern u8 IR_500ms;
